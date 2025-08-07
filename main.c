24f11436508418f09252e064f181dfa3e2d4a9c9
PaleEXE <150634604+PaleEXE@users.noreply.github.com>
2025-08-07 13:52:47 +0300

    Update Main
    Refactor code and add interactive stuff on click 🌷.

--- main.c
+++ main.c
@@ -1,37 +1,44 @@
-#include "raylib.h"
+#include "include/raylib.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+#define SPEED 400
 
 int main(void)
 {
     Texture2D sprite;
     Sound sound;
     Music music;
-    
-    InitWindow(800, 450, "asgore window");
+
+    InitWindow(WIDTH, HEIGHT, "asgore window");
     
     InitAudioDevice();
     
-    sprite = LoadTexture("asgore.jpeg");
-    sound = LoadSound("car-engine-roaring-376881.mp3");
-    music = LoadMusicStream("Asgore-Runs-Over-Dess.wav");
+    sprite = LoadTexture("assets/img/asgore.jpeg");
+    sound = LoadSound("assets/sound/car-engine-roaring-376881.mp3");
+    music = LoadMusicStream("assets/sound/Asgore-Runs-Over-Dess.wav");
     PlayMusicStream(music);
     
-    float posX = -sprite.width;
+    float posX = WIDTH / 2.0f - sprite.width / 2.0f;
+    float posY = HEIGHT / 2.0f - sprite.height / 2.0f;
+    float dx = 1, dy = 1;
     
     while (!WindowShouldClose())
     {
         UpdateMusicStream(music);
         
-        posX += GetFrameTime() * 300;
-        if (posX > 800)
-        {
-            posX = -sprite.width;
-        }
-        
+        posX += GetFrameTime() * SPEED * dx;
+        posY += GetFrameTime() * SPEED * dy;
+        if (posX + sprite.width > WIDTH)   dx = -1;
+        if (posY + sprite.height > HEIGHT) dy = -1;
+        if (posX < 0.0f) dx = 1;
+        if (posY < 0.0f) dy = 1;
+
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
             Rectangle spriteRect = {
                 posX,
-                10,
+                posY,
                 sprite.width,
                 sprite.height
             };
@@ -39,6 +46,8 @@ int main(void)
             if (CheckCollisionPointRec(GetMousePosition(), spriteRect))
             {
                 PlaySound(sound);
+                dx *= -1.1;
+                dy *= -1.1;
             }
         }
         
@@ -46,7 +55,7 @@ int main(void)
         
             ClearBackground(RAYWHITE);
             
-            DrawTexture(sprite, posX, 10, WHITE);
+            DrawTexture(sprite, posX, posY, WHITE);
             
         EndDrawing();
     }
